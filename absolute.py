101c3fbb475ffaab4d7a5fdb14c10584ab1e6f5f
HuuBaa <742790905@qq.com>
2018-10-27 20:04:28 +0800

    fix

--- absolute.py
+++ absolute.py
@@ -3,16 +3,17 @@ __author__ = 'Huu'
 __date__ = '2018/10/20 15:55'
 
 import sys
-from PyQt5.QtWidgets import QLabel,QApplication,QMainWindow
+from PyQt5.QtWidgets import QLabel, QApplication, QMainWindow
+
 
 class Example(QMainWindow):
     def __init__(self):
         super().__init__()
         self.initUI()
-    def initUI(self):
 
-        label1=QLabel('label 1',self)
-        label1.move(10,10)
+    def initUI(self):
+        label1 = QLabel('label 1', self)
+        label1.move(10, 10)
 
         label2 = QLabel('label 2', self)
         label2.move(30, 30)
@@ -21,11 +22,11 @@ class Example(QMainWindow):
         label3.move(60, 60)
 
         self.setWindowTitle('absolute')
-        self.setGeometry(300,300,300,300)
+        self.setGeometry(300, 300, 300, 300)
         self.show()
 
 
-if __name__=='__main__':
-    app=QApplication(sys.argv)
-    ex=Example()
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
     sys.exit(app.exec_())
